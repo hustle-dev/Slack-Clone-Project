d6696e59bfddf1510a951b55031c172d75624241
hustle-dev <dlwoabsdk@gmail.com>
2022-01-24 14:29:22 +0900

    feat ChatList 컴포넌트 생성

---
+++ frontend/src/components/ChatList/ChatList.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { ChatZone, Section } from './ChatList.styles';
+
+export default function ChatList() {
+  return (
+    <ChatZone>
+      <Section>section</Section>
+    </ChatZone>
+  );
+}
